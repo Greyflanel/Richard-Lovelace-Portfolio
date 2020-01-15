8357ff170d7756014ad9c08c6437e601b6dea295
Greyflanel <richardlovelace811@gmail.com>
2020-01-15 04:05:58 -0600

    Practicing greensock animation

---
+++ index.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 6 */
+const hello = document.querySelector('.hello');
+    gsap.to(hello, 5, { scaleY:'5'});
+ const helloX = document.querySelector('.hello');   
+     gsap.to(hello, 6, { color: 'red',
+                    backgroundColor: "green"});
+// const hello = document.querySelector('.hello');
+// let tween = gsap.to(hello, { yoyo: "true"}, 0);
