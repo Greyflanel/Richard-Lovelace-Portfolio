ee2820054a40bca7bfe7faf649c14bfbf7ca6920
Greyflanel <richardlovelace811@gmail.com>
2020-01-18 07:49:16 -0600

    Finishing up some cool animation.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /* jshint esversion: 6 */
-const helloAgain = document.querySelector("h1");
-gsap.to(helloAgain, 15, {  textShadow: "3px 3px 6px #f7f8d2", color: "#0a0111", runBackwards: "true"});
 
+gsap.to("#me", 25, {  textShadow: "3px 4px 5px 6px #f7f8d2", color: "#0a0111", runBackwards: "true"});
+gsap.to("#mybutton", 25, {    borderColor: "#22162a",  boxShadow: "2px 2px 2px 2px #22162a", runBackwards: "true"});
 const hello = document.querySelector("h2");
-gsap.to(hello, 15, { textShadow: "3px 3px 6px #f7f8d2", color: "#0a0111", runBackwards: "true"});
+gsap.to("#you", 25, { textShadow: "2px 2px 2px 4px #f7f8d2", color: "#0a0111", runBackwards: "true"});
+
 
 // const helloStranger = document.querySelector("h1");
 // gsap.to(helloStranger, 8, {  fontSize: 4.5, runBackwards: "true" });
@@ -11,7 +12,7 @@ gsap.to(hello, 15, { textShadow: "3px 3px 6px #f7f8d2", color: "#0a0111", runBac
 // const helloGoodbye = document.querySelector("h2");
 // gsap.to(helloGoodbye, 8, {  fontSize: 3.3, runBackwards: "true"});
 
-let tween = gsap.to(".moon", { duration: 40, x: -850, y: -130, runBackwards: "true"});
+let tween = gsap.to(".moon", { duration: "40", x: "-900", y: "-100", ease: "SlowMo.out", runBackwards: "true",});
 
 
 // var tween = gsap.to("#content", {
