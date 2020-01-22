fbbb5a63554def819286b398ecc2719ef465cd4f
Greyflanel <richardlovelace811@gmail.com>
2020-01-22 06:04:41 -0600

    Just about done with moon animation and thinking of keeping flickering neon light effect.

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 /* jshint esversion: 6 */
 
-// gsap.to("#me", 8, {  textShadow: "0px 0px 10px  rgb(255, 230, 255)", color: "rgb(10, 1, 17, 10)", runBackwards: "true" });
-// gsap.to("#we", 8, { textShadow: "0px 0px 10px  rgb(255, 230, 255)", color: "rgb(10, 1, 17, 10)", runBackwards: "true"});
-// gsap.to("#mybutton", 20, {  boxShadow: "0px 0px 10px  rgb(255, 230, 255)", color: "rgb(10, 1, 17, 10)", });
+// let tween = gsap.to(".moon", { duration: "15", x: "1230", y: "-250", ease: "power1.out",  });
+
+var tl = gsap.timeline({repeat: 200});
+tl.set(".moon", { left: "5rem", bottom: "6rem"})
+tl.to(".moon", { x: 300, y: -400, duration: 13, ease: "linear"});
+tl.to(".moon",  {x: 505, y: -435, duration: 10, ease: "linear"});
+tl.to(".moon", {x: 800, y: -455, duration: 11, ease: "linear"});
+tl.to(".moon",  {x: 980, y: -285, duration: 12, ease: "linear"});
+tl.to(".moon",  {x: 1050, y: -100, duration: 14, ease: "linear"});
+tl.to(".moon",  {x: 830, y: -50, duration: 15, ease: "linear"});
+tl.to(".moon",  {x: 500, y: -40, duration: 16, ease: "linear"});
+tl.to(".moon",  {x: 0, y: 0, duration: 16, ease: "linear"});
 
 
-let tween = gsap.to(".moon", { duration: "55", x: "1200", y: "-200", ease: "power1.out",  });
 
 
-// var tween = gsap.to("#content", {
-//   duration: 4,
-//   x: 750,
-//   rotation: 360,
-//   ease: "none",
-//   paused: true
-// });
 
-// document.querySelector("#play").onclick = () => tween.play();
-// document.querySelector("#pause").onclick = () => tween.pause();
-// document.querySelector("#resume").onclick = () => tween.resume();
-// document.querySelector("#reverse").onclick = () => tween.reverse();
-// document.querySelector("#restart").onclick = () => tween.restart();
+
+tl.from("#me", {opacity: 0,  duration: 0.212, repeat: 6, ease: "rough.in"}, "-=");
+tl.to("#me", {opacity: 1,  duration: 0.14, repeat: 3, delay: 2}, "-=");
+tl.from("#me", {opacity: 0,  duration: 0.14, repeat: 1 }, "-=");
+tl.from("#we", {opacity: 0,  duration: 0.156, repeat: 0.2, delay: 1, ease: "rough.in"}, "-=");
+tl.to("#we", {opacity: 1,  duration: 0.24, repeat: 3, delay: 0.5}, "-=");
+tl.from("#we", {opacity: 0,  duration: 0.4, repeat: 1, delay: 0.1 }, "-=");
+
+// tl.pause();
+// tl.resume();
+// tl.seek(30);
+// tl.play();
+// tl.reverse();
+
+
