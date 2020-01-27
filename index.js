977e38ecb4dbaf34d3c0450d893963de5b72c98c
Greyflanel <richardlovelace811@gmail.com>
2020-01-27 05:19:44 -0600

    doing some detail work on shadows and animation timing. Working on smaller screen sizes now.

--- index.js
+++ index.js
@@ -2,31 +2,89 @@
 
 // let tween = gsap.to(".moon", { duration: "15", x: "1230", y: "-250", ease: "power1.out",  });
 
-var tl = gsap.timeline({repeat: 200});
-tl.set(".moon", { left: "5rem", bottom: "6rem"});
-tl.to(".moon", { x: 300, y: -400, duration: 13, ease: "linear"});
-tl.to(".moon",  {x: 505, y: -435, duration: 10, ease: "linear"});
-tl.to(".moon", {x: 800, y: -455, duration: 11, ease: "linear"});
-tl.to(".moon",  {x: 980, y: -285, duration: 12, ease: "linear"});
-tl.to(".moon",  {x: 1050, y: -100, duration: 14, ease: "linear"});
-tl.to(".moon",  {x: 830, y: -40, duration: 15, ease: "linear"});
-tl.to(".moon",  {x: 500, y: -10, duration: 16, ease: "linear"});
-tl.to(".moon",  {x: 0, y: 0, duration: 18, ease: "linear"});
+
+
+var mqls = [
+    window.matchMedia("(min-width: 1200px)"),
+    window.matchMedia("(max-width: 1000px)"),
+    window.matchMedia("(max-width: 800px)"),
+    window.matchMedia("(max-width: 600px)"),
+    window.matchMedia("(max-width: 400px)"),
+    window.matchMedia("(max-width: 300px)"),
+];
+ 
+function mediaqueryresponse(mql){
+    let tl = gsap.timeline({repeat: 200});
+    
+    if(mqls[0].matches){
+tl.set(".moon", { x: 140, y: 100, });        
+tl.to(".moon",  { x: 300, y: 320, duration: 20, ease: "linear"});
+tl.to(".moon",  {x: 500, y: 400, duration: 18, ease: "linear"});
+tl.to(".moon",  {x: 1000, y: 480, duration: 32, ease: "linear"});
+tl.to(".moon",  {x: 1200, y: 50, duration: 30, ease: "linear"});
+tl.to(".moon",  {x: 140, y: 100, duration: 44, ease: "linear"});
+    }
+  if(mqls[1].matches){ 
+tl.set(".moon", { x: 140, y: 100, });        
+tl.to(".moon",  { x: 160, y: 250, duration: 20, ease: "linear"});
+tl.to(".moon",  {x: 280, y: 350, duration: 18, ease: "linear"});
+tl.to(".moon",  {x: 650, y: 480, duration: 24, ease: "linear"});
+tl.to(".moon",  {x: 720, y: 150, duration: 20, ease: "linear"});
+tl.to(".moon",  {x: 140, y: 100, duration: 32, ease: "linear"});
+    }
+
+if(mqls[2].matches){
+tl.to(".moon", { x: 300, y: 400, duration: 13, ease: "linear"});
+tl.to(".moon",  {x: 505, y: 435, duration: 10, ease: "linear"});
+tl.to(".moon", {x: 800, y: 455, duration: 11, ease: "linear"});
+tl.to(".moon",  {x: 980, y: 485, duration: 12, ease: "linear"});
+tl.to(".moon",  {x: 1050, y: 400, duration: 14, ease: "linear"});
+tl.to(".moon",  {x: 830, y: 300, duration: 15, ease: "linear"});
+tl.to(".moon",  {x: 500, y: 400, duration: 16, ease: "linear"});
+tl.to(".moon",  {x: 100, y: 400, duration: 18, ease: "linear"});
+    }
+    if(mqls[3].matches){
+tl.to(".moon", { x: 300, y: 400, duration: 13, ease: "linear"});
+tl.to(".moon",  {x: 505, y: 435, duration: 10, ease: "linear"});
+tl.to(".moon", {x: 800, y: 455, duration: 11, ease: "linear"});
+tl.to(".moon",  {x: 980, y: 285, duration: 12, ease: "linear"});
+tl.to(".moon",  {x: 1050, y: 400, duration: 14, ease: "linear"});
+tl.to(".moon",  {x: 830, y: 450, duration: 15, ease: "linear"});
+tl.to(".moon",  {x: 500, y: 350, duration: 16, ease: "linear"});
+tl.to(".moon",  {x: 100, y: 400, duration: 18, ease: "linear"});
+    }
+    if(mqls[4].matches){
+tl.to(".moon", { x: 300, y: 400, duration: 1, ease: "linear"});
+tl.to(".moon",  {x: 505, y: 435, duration: 1, ease: "linear"});
+tl.to(".moon", {x: 800, y: 455, duration: 1, ease: "linear"});
+tl.to(".moon",  {x: 980, y: 485, duration: 1, ease: "linear"});
+tl.to(".moon",  {x: 1050, y: 400, duration: 1, ease: "linear"});
+tl.to(".moon",  {x: 830, y: 500, duration: 1, ease: "linear"});
+tl.to(".moon",  {x: 500, y: 500, duration: 1, ease: "linear"});
+tl.to(".moon",  {x: 100, y: 400, duration: 1, ease: "linear"});
+ }
+}
+
+if(mqls[5].matches){
+tl.to(".moon", { x: 200, y: 400, duration: 13, ease: "linear"});
+tl.to(".moon",  {x: 105, y: 435, duration: 10, ease: "linear"});
+tl.to(".moon", {x: 300, y: 555, duration: 11, ease: "linear"});
+}
+ 
+for (let i=0; i<mqls.length; i++){
+    mediaqueryresponse(mqls[i]);
+    mqls[i].addListener(mediaqueryresponse); 
+}
+// let mql = window.matchMedia('(max-width: 200px)');
+
+// document.querySelector(".mq-value").innerText = mql.matches;
 
 // let scroll = gsap.to(window, 6, {scrollTo:{y:".second-section"}}, "<");
 // scroll();
 
-
-// tl.from("#me", {opacity: 0,  duration: 0.212, repeat: 6, ease: "rough.in"}, "-=");
-// tl.to("#me", {opacity: 1,  duration: 0.8, repeat: 3, delay: 2}, "-=");
-// tl.from("#me", {opacity: 0,  duration: 0.164555, repeat: 4 }, "-=");
-// tl.from("#we", {opacity: 0,  duration: 0.15445555, repeat: 0.5, delay: 1, ease: "rough.in"}, "-=");
-// tl.to("#we", {opacity: 1,  duration: 0.245544, repeat: 4, delay: 0.5}, "-=");
-// tl.from("#we", {opacity: 0,  duration: 0.2555, repeat: 1, delay: 0.1224 }, "-=");
-
 // tl.pause();
 // tl.resume();
-// tl.seek(30);
+// tl.seek(50);
 // tl.play();
 // tl.reverse();
 
