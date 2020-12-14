f6ea510dcf89756751b93a12d74cb60bcc5c1948
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-12-13 21:06:20 -0600

    new stars

--- index.js
+++ index.js
@@ -30,51 +30,47 @@ function mediaqueryresponse(mql) {
   let tl4 = gsap.timeline({ repeat: 5 });
   let tl5 = gsap.timeline({ repeat: 5 });
   
-  
 
-  // tl2.set(".spotlight", { filter: "blur(2em), brightness(150%)" });
-  // tl2.to(".spotlight", { filter: "blur(2em),brightness(250%)", duration: 5, ease: "linear" });
-  // tl2.to(".spotlight", { filter: "blur(2em), brightness(200%)", duration: 25, ease: "linear" });
 
-  tl3.set(".me", {
-    x: 0,
-    y: 0,
-    fontSize: "2rem",
-    opacity: 0,
-    rotate: 0,
-    delay: 0,
-  });
+  // tl3.set(".me", {
+  //   x: 0,
+  //   y: 0,
+  //   fontSize: "2rem",
+  //   opacity: 0,
+  //   rotate: 0,
+  //   delay: 0,
+  // });
 
-  tl3.to(".me", {
-    fontSize: "5rem",
-    opacity: 1,
-    duration: 3,
-    ease: "linear",
-  });
-  tl3.to(".me", {
-    x: 0,
-    y: 120,
-    opacity: "-1",
-    duration: 4,
-    delay: 3,
-    ease: "power1.inOut",
-  });
+  // tl3.to(".me", {
+  //   fontSize: "5rem",
+  //   opacity: 1,
+  //   duration: 3,
+  //   ease: "linear",
+  // });
+  // tl3.to(".me", {
+  //   x: 0,
+  //   y: 120,
+  //   opacity: "-1",
+  //   duration: 4,
+  //   delay: 3,
+  //   ease: "power1.inOut",
+  // });
 
-  tl4.set(".we", { opacity: "0", rotate: 0 });
+  // tl4.set(".we", { opacity: "0", rotate: 0 });
 
-  tl4.to(".we", {
-    opacity: "1",
-    duration: 4,
-    ease: "linear",
-  });
-  tl4.to(".we", {
-    fontSize: "1rem",
-    opacity: "-1",
-    rotate: -360,
-    duration: 4,
-    ease: "power1.inOut",
-    delay: 2,
-  });
+  // tl4.to(".we", {
+  //   opacity: "1",
+  //   duration: 4,
+  //   ease: "linear",
+  // });
+  // tl4.to(".we", {
+  //   fontSize: "1rem",
+  //   opacity: "-1",
+  //   rotate: -360,
+  //   duration: 4,
+  //   ease: "power1.inOut",
+  //   delay: 2,
+  // });
 
   
 
@@ -95,54 +91,107 @@ function mediaqueryresponse(mql) {
       ease: "linear",
     });
   } else if (mqls[1].matches) {
-    tl.set(".moon", { x: 140, y: 100 });
+    tl.set(".moon, .spotlight", { x: 140, y: 100 });
 
-    tl.to(".moon", { x: 160, y: 250, duration: 30, ease: "linear" });
-    tl.to(".moon", { x: 280, y: 350, duration: 30, ease: "linear" });
-    tl.to(".moon", { x: 550, y: 720, duration: 30, ease: "linear" });
-    tl.to(".moon", { x: 720, y: 150, duration: 30, ease: "linear" });
-    tl.to(".moon", { x: 140, y: 100, duration: 30, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 160, y: 250, duration: 30, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 280, y: 350, duration: 30, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 550, y: 720, duration: 30, ease: "linear" });
+    tl.to(".moon .spotlight", { x: 720, y: 150, duration: 30, ease: "linear" });
+    tl.to(".moon .spotlight", { x: 140, y: 100, duration: 30, ease: "linear" });
   } else if (mqls[2].matches) {
-    tl.set(".moon", { x: 140, y: 100 });
+    tl.set(".moon, .spotlight", { x: 140, y: 100 });
 
-    tl.to(".moon", { x: 160, y: 200, duration: 20, ease: "linear" });
-    tl.to(".moon", { x: 180, y: 300, duration: 18, ease: "linear" });
-    tl.to(".moon", { x: 450, y: 400, duration: 24, ease: "linear" });
-    tl.to(".moon", { x: 520, y: 150, duration: 20, ease: "linear" });
-    tl.to(".moon", { x: 140, y: 100, duration: 32, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 160, y: 200, duration: 20, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 180, y: 300, duration: 18, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 450, y: 400, duration: 24, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 520, y: 150, duration: 20, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 140, y: 100, duration: 32, ease: "linear" });
   } else if (mqls[3].matches) {
-    tl.set(".moon", { x: 40, y: 80 });
+    tl.set(".moon, .spotlight", { x: 40, y: 80 });
 
-    tl.to(".moon", { x: 350, y: 400, duration: 45, ease: "linear" });
-    tl.to(".moon", { x: 520, y: 115, duration: 45, ease: "linear" });
-    tl.to(".moon", { x: 40, y: 80, duration: 50, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 350, y: 400, duration: 45, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 520, y: 115, duration: 45, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 40, y: 80, duration: 50, ease: "linear" });
   } else if (mqls[4].matches) {
     tl.set(".moon", { x: 40, y: 80 });
-
-    tl.to(".moon", { x: 50, y: 150, duration: 10, ease: "linear" });
-    tl.to(".moon", { x: 90, y: 200, duration: 6, ease: "linear" });
-    tl.to(".moon", { x: 180, y: 280, duration: 9, ease: "linear" });
-    tl.to(".moon", { x: 240, y: 90, duration: 12, ease: "linear" });
-    tl.to(".moon", { x: 40, y: 80, duration: 12, ease: "linear" });
+    tl.set(".spotlight", { x: 50, y: 80 });
+    tl.to(".moon, .spotlight", { x: 50, y: 150, duration: 10, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 90, y: 200, duration: 6, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 180, y: 280, duration: 9, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 240, y: 90, duration: 12, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 40, y: 80, duration: 12, ease: "linear" });
   } else if (mqls[5].matches) {
     tl.set(".moon", { x: 20, y: 80 });
-
-    tl.to(".moon", { x: 180, y: 70, duration: 20, ease: "linear" });
-    tl.to(".moon", { x: 200, y: 300, duration: 45, ease: "linear" });
-    tl.to(".moon", { x: 22, y: 230, duration: 25, ease: "linear" });
-    tl.to(".moon", { x: 150, y: 150, duration: 15, ease: "linear" });
-    tl.to(".moon", { x: 20, y: 80, duration: 15, ease: "linear" });
+    tl.set(".spotlight", { x: 50, y: 80})
+    tl.to(".moon, .spotlight", { x: 180, y: 70, duration: 20, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 200, y: 300, duration: 45, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 22, y: 230, duration: 25, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 150, y: 150, duration: 15, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 20, y: 80, duration: 15, ease: "linear" });
   } else if (mqls[6].matches) {
     tl.set(".moon", { x: 20, y: 80 });
-
-    tl.to(".moon", { x: 120, y: 100, duration: 20, ease: "linear" });
-    tl.to(".moon", { x: 130, y: 300, duration: 45, ease: "linear" });
-    tl.to(".moon", { x: 22, y: 230, duration: 25, ease: "linear" });
-    tl.to(".moon", { x: 120, y: 150, duration: 15, ease: "linear" });
-    tl.to(".moon", { x: 20, y: 80, duration: 15, ease: "linear" });
+    tl.set(".spotlight", { x: -50, y: 80})
+    tl.to(".moon, .spotlight", { x: 120, y: 100, duration: 20, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 130, y: 300, duration: 45, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 22, y: 230, duration: 25, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 120, y: 150, duration: 15, ease: "linear" });
+    tl.to(".moon, .spotlight", { x: 20, y: 80, duration: 15, ease: "linear" });
   }
 }
 for (let i = 0; i < mqls.length; i++) {
   mediaqueryresponse(mqls[i]);
-  mqls[i].addEventListener(mediaqueryresponse);
+  mqls[i].addEventListener(mediaqueryresponse, {});
 }
+
+let scene, camera, renderer, sphere, wireframe, line;
+
+function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.SphereGeometry(9, 50, 50);
+
+  sphere = new THREE.Mesh(geometry, wireframe);
+
+  wireframe = new THREE.WireframeGeometry(geometry);
+  line = new THREE.LineSegments(wireframe);
+  line.material.depthTest = false;
+  line.material.transparent = false;
+
+  scene.add(line);
+
+  camera.position.z = 15;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  line.rotation.x += 0.01;
+  line.rotation.y -= 0.01;
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onWindowResize, false);
+
+init();
+animate();
