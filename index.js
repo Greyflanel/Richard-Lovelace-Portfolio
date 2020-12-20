43c896fd07283844200df4185c312b31f83f4b2c
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-12-20 15:26:31 -0600

    new animation changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var mqls = [
-  window.matchMedia("(min-width: 1200px)"),
+  window.matchMedia("(min-width: 1281px)"),
   window.matchMedia("(min-width: 1000px)"),
   window.matchMedia("(min-width: 800px)"),
   window.matchMedia("(min-width: 600px)"),
@@ -9,95 +9,95 @@ var mqls = [
 ];
 
 function mediaqueryresponse(mql) {
-  let tl = gsap.timeline({ repeat: 1000 });
+  let tl = gsap.timeline({ repeat: -1 });
   let tl2 = gsap.timeline({ repeat: 1000 });
   let tl3 = gsap.timeline({ repeat: 5 });
   let tl4 = gsap.timeline({ repeat: 5 });
   let tl5 = gsap.timeline({ repeat: 5 });
 
+  tl2.to(".content", {
+    delay: 4,
+    filter: "brightness(2%)",
+    duration: 2,
+  });
+
+  tl2.to(".content", {
+    delay: 2,
+    filter: "brightness(75%)",
+    duration: 4,
+  });
+
+  tl2.to(".content", {
+    delay: 4,
+    filter: "brightness(120%)",
+    duration: 2,
+  });
+
+  tl2.to(".content", {
+    delay: 1,
+    filter: "brightness(75%)",
+    duration: 4,
+  });
+
+  tl2.to(".content", {
+    delay: 0.4,
+    filter: "brightness(2%)",
+    duration: 6,
+  });
+
   tl.set(".moon, .spotlight", {
     xPercent: -50,
     yPercent: -50,
-    x: -650,
-    y: 180,
+    x: 0,
+    y: 0,
   });
-  
-  
+
   if (mqls[0].matches) {
-    tl2.to(".content", {
-      delay: 4, 
-      filter: "brightness(2%)",
-      duration: 2,
-    });
-    tl2.to(".content", {
-      delay: 2,
-      filter: "brightness(75%)",
-      duration: 4
-    });
-    tl2.to(".content", {
-      delay: 4,
-      filter: "brightness(120%)",
-      duration: 2,
-    });
-    tl2.to(".content", {
-      delay: 1,
-      filter: "brightness(75%)",
-      duration: 4,
-    });
-    tl2.to(".content", {
-      delay: 0.4,
-      filter: "brightness(2%)",
-      duration: 6,
-    });
+
     tl.to(".moon, .spotlight", {
       x: "+=650",
       y: "-=400",
       duration: 30,
       ease: "linear",
     });
-    
+
+    tl.to(
+      ".moon, .spotlight",
+      {
+        x: "+=649",
+        y: "+=400",
+        duration: 30,
+        ease: "linear",
+      }
+    );
+
     tl.to(".moon, .spotlight", {
-      x: "+=649",
-      y: "+=400",
-      duration: 30,
-      ease: "linear",
-    }, "+=4");
-    
-    tl.to(".moon, .spotlight", {
-      x: "-=1300",
+      x: "-=50%",
       y: "+=0",
       duration: 56,
       ease: "linear",
     });
-   
-  } else if (mqls[1].matches) {
-    tl.set(".moon, .spotlight", { x: 140, y: 100 });
 
-    tl.to(".moon, .spotlight", {
-      x: 160,
-      y: 250,
-      duration: 16,
+  } else if (mqls[1].matches) {
+    tl.set(".moon, .spotlight", {
+      width: "22rem",
+      x: "-=120%",
+      y: "+=30%"
+    }).to(".moon, .spotlight", {
+      x: "+=120%",
+      y: "-=50%",
+      duration: 8,
+      ease: "linear",
+    }).to(".moon, .spotlight", {
+      x: "+=120%",
+      y: "+=50%",
+      duration: 8,
       ease: "linear",
     });
-    tl.to(".moon, .spotlight", {
-      x: 280,
-      y: 350,
-      duration: 30,
-      ease: "linear",
-    });
-    tl.to(".moon, .spotlight", {
-      x: 550,
-      y: 720,
-      duration: 30,
-      ease: "linear",
-    });
-    tl.to(".moon .spotlight", { x: 720, y: 150, duration: 30, ease: "linear" });
-    tl.to(".moon .spotlight", {
-      x: 140,
-      y: 100,
-      duration: 30,
-      ease: "linear",
-    });
+    
+   
+    
+    
   } else if (mqls[2].matches) {
     tl.set(".moon, .spotlight", { x: 140, y: 100 });
 
