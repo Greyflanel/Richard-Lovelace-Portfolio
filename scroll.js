c199b1d1ddfa4a6c6591f6e0562985a3f10bbaf7
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-01-30 19:40:36 -0600

    try again

--- scroll.js
+++ scroll.js
@@ -7,86 +7,54 @@ function playVideo(el) {
   console.log("playing video");
 };
 
-function pauseVideo(el) {
-  let vid = document.getElementById(el);
-  vid.pause();
-  console.log("pausing video");
-};
+// function pauseVideo(el) {
+//   let vid = document.getElementById(el);
+//   vid.pause();
+//   console.log("pausing video");
+// };
 
 let timeline = gsap.timeline({
   repeat: 0,
+  scrollTrigger: "body",
 });
 let timeline2 = gsap.timeline({
   repeat: 0,
+  scrollTrigger: "body",
 });
 
 
 function scrollAnimation() {
   
-  timeline.set(".smoke-vid", {visibility: "hidden", opacity: 1, y: 0});
-  timeline2.set(".smoke-text", {visibility: "hidden", opacity: 0, y: 0});
-  timeline
-    .to(".smoke-vid", {
-      autoAlpha: 1,
-      scrollTrigger: {
-        trigger: "#fourth-section",
-        scroller: "body",
-        scrub: true,
-        ease: "power1-inOut",
-        start: "top top", //when top  passes the bottom viewport height
-        onToggle: (self) => {console.log(self)
-          self.isActive ? playVideo("smokey") : console.log("Paused")}
-        ,
-        //events: onEnter onLeave onEnterBack onLeaveBack
-        toggleActions: "restart complete reverse reset",
-        //options: play, pause, resume, reset, restart, complete, reverse,none
-      },
-    })
-    .to(".smoke-vid", {
-      y: 70,
-      duration: 8,
-    }, "2.5")
-    .to(".smoke-vid", {
-      autoAlpha: 0,
-      duration: 2,
-    }, "-=4")
-    
-}
-
-function textScrollAnimation() {
+  timeline.set(".smoke-vid", {visibility: "hidden", opacity: 1}).set(".smoke-text", { 
+    opacity: 0,
+    repeat: 0
+  });
   
-  timeline2.set(".smoke-text", { visibility: "hidden", opacity: 0, y: 0 });
-  timeline2
-    .to(".smoke-text", {
-      
-      duration: 6,
+let smokeAnimation = ScrollTrigger.batch(".smoke-vid, .smoke-text", {
+  interval: 0.1, // time window (in seconds) for batching to occur.
+  batchMax: 3, // maximum batch size (targets). Can be function-based for dynamic values
+
+  onEnter: (batch) =>
+    timeline.to(batch, {
+      opacity: 0.5,
       autoAlpha: 1,
-      scrollTrigger: {
-        trigger: "#fourth-section",
-        scroller: "body",
-        scrub: true,
-        ease: "power1-inOut",
-        start: "top top", //when top  passes the bottom viewport height
-        
-      },
-    }, "4")
-    .to(
-      ".smoke-text",
-      {
-        y: 70,
-        duration: 3,
-      },
+      onToggle: playVideo("smokey"),
       
-    )
-    .to(
-      ".smoke-text",
-      {
-        autoAlpha: 0,
-        duration: 2,
-      },
-      "-=4"
-    );
+      y: 60,
+      duration: 5,
+      stagger: { each: 1.8 , grid: [1, 20] },
+      ease: "power1.in",
+      duration: 5,
+    }).to(batch, {
+      autoAlpha: 0,
+      duration: 0.8,
+      
+    }).to(".smoke-vid, .smoke-text", {
+      display: "none"
+    }),
+  
+});
+smokeAnimation.kill();
 }
 
 scrollAnimation();
-textScrollAnimation();
