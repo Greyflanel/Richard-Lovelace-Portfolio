f69510fcf1ad1855449a3f31dde8d1eca1f004f1
Greyflanel <richardlovelace811@gmail.com>
2021-02-01 17:08:27 -0600

    changing up logic for animation

--- scroll.js
+++ scroll.js
@@ -14,47 +14,37 @@ function playVideo(el) {
 // };
 
 let timeline = gsap.timeline({
-  repeat: 10,
-  scrollTrigger: "body",
+  repeat: 0,
+  onStart: playVideo("smokey"),
+  
 });
 let timeline2 = gsap.timeline({
-  repeat: 0,
-  scrollTrigger: "body",
+  
+  
 });
 
 
 function scrollAnimation() {
   
-  timeline.set(".smoke-vid", {visibility: "hidden", opacity: 1}).set(".smoke-text", { 
-    opacity: 0,
-    repeat: 0
-  });
   
-let smokeAnimation = ScrollTrigger.batch(".smoke-vid, .smoke-text", {
-  interval: 0.1, // time window (in seconds) for batching to occur.
-  batchMax: 3, // maximum batch size (targets). Can be function-based for dynamic values
-
-  onEnter: (batch) =>
-    timeline.call
-    (playVideo,['smokey']).to(batch, {
-      opacity: 0.5,
-      autoAlpha: 1,
-      
-      
-      y: 60,
-      duration: 5,
-      stagger: { each: 1.8 , grid: [1, 20] },
-      ease: "power1.in",
-      duration: 5,
-    }).to(batch, {
-      autoAlpha: 0,
-      duration: 0.8,
-      
-    }).to(".smoke-vid, .smoke-text", {
-      display: "none"
-    }),
-  
-});
+  timeline.set(".smoke-vid, .smoke-text", {
+    opacity: 0
+  }).to(".smoke-vid", {
+    duration: 3,
+    autoAlpha: 1,
+    y: 60
+  }).to(".smoke-text", {
+    
+    delay: 5,
+    duration: 12,
+    autoAlpha: 1,
+    y: 60,
+  }, "-=6").to(".smoke-vid", {
+    
+    autoAlpha: 0
+  }, "-=2.5").to(".smoke-text", {
+    autoAlpha: 0
+  })
 
 }
 
